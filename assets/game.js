8a04be8aaefcfcc31d41738cd115979d6c3b4027
Carlo Manalese <CarloManalese@carlos-mbp.attlocal.net>
2018-02-05 00:39:12 -0800

    updated

--- assets/game.js
+++ assets/game.js
@@ -1,58 +1,66 @@
-// alert("Want to play a psychic game?")
+
 
 var answer=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var computerChoice;
-var i= (0);
-var wins= 0;
-var losses= 0;
-var guess= 10;
+var i=(0);
+var wins=0;
+var losses=0;
+var guesses=10;
 var yourGuess=[];
+var words= [];
 
 
 	// var guess=prompt("guess a letter")
 
 	function choices(){
-		
-		computerChoice=answer[Math.floor(Math.random()*26+1)];
-		console.log(computerChoice);
+		computerChoice=answer[Math.floor(Math.random()*26) +1]
+	console.log(computerChoice)
 	}
 
-	document.onkeyup = function(event){
-		yourGuess = event.key;
-		choices ();
+	choices()
+ 
+ 	document.onkeyup = function(event){
+ 		yourGuess = event.key;
+ 		console.log(yourGuess);
+ 	
 
-	if (yourGuess === computerChoice){
+
+
+		if(yourGuess === computerChoice){
 		wins === wins++;
 		guess = 10;
-		yourGuess = [];
-		// console.log(wins)
-	}
+		words = [];
+		console.log(wins)
+		choices()
+		}
 
-	else {
-		guess === guess--;
-		guess = guess + yourGuess + ",";
-	}
+		else {
+			guesses == guesses--;
+			words = words + yourGuess + ","
+		}
 
-	if (guess === 0){
-		losses === losses++;
-		guess = 10;
-		yourGuess = [];
-		choices ();
-	}
-
-
-
-
-
+	    if(guesses=== 0){
+	    	losses === losses++;
+	    	guesses = 10;
+	    	words = [];
+	    	choices();
+	    }
 		
+		
+
 		
 
 	document.querySelector("#wins").innerHTML = "wins:"+ wins;
+	
 	document.querySelector("#losses").innerHTML = "losses:" + losses;
-	document.querySelector("#guess").innerHTML= "guess:" + guess; 
-	document.querySelector("#yourGuess").innerHTML= "yourGuess:" + yourGuess;
+	
+	document.querySelector("#guess").innerHTML= "guesses:" + guesses; 
+	
+	document.querySelector("#yourGuess").innerHTML= "yourGuess:" + words;
 
-	}//end of onkeyup function.	
+		}
+
+	
 		
 	// document.querySelector("#wins").innerHTML "wins:"+ wins++;
 	// document.querySelector("#losses").innerHTML "lossses:" + losses++;
